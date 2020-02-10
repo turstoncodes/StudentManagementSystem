49eab6983e0ec17dfb77902e988fffb04459d11b
Turston Louw <53752666+turstoncodes@users.noreply.github.com>
2020-02-10 20:23:29 +0200

    Update student.py

--- student.py
+++ student.py
@@ -2,7 +2,7 @@ import os
 import platform
 
 global listStd #Making ListStd As Super Global Variable
-listStd = ["yugesh", "kishor", "gajen", "Gopi"] #List Of Students
+listStd = ["kezia", "cindy", "ashley", "amy"] #List Of Students
 
 def manageStudent(): #Function For The Student Management System
 
